cf4023ab98d1a1df9304c6a0a673d5be63bc615d
nickridwan <ridwanhanif165@gmail.com>
2023-01-26 10:13:26 +0700

    change token

--- lib/utils/settings.dart
+++ lib/utils/settings.dart
@@ -1,3 +1,3 @@
 const APP_ID = '6a944e55a80443c58f6c033db4ff832a';
 const Token =
-    '007eJxTYBA70OT0c+kW2TkcBT1TUz41bv5h1bNNkEVgTrfS9MOreh8qMJglWpqYpJqaJloYmJgYJ5tapJklGxgbpySZpKVZGBslirFeTG4IZGRg4vRhYmSAQBCfhSEqPz+XgQEANCwd6g==';
+    '007eJxTYFjomr+P//3i6pkbbCufz/KeXW40f8f87zUZr28JWtxeWqurwGCWaGlikmpqmmhhYGJinGxqkWaWbGBsnJJkkpZmYWyUKPf2YnJDICPDp3R2BkYoBPF5GKry83MVclNTSzLz0hkYAOysJD0=';
